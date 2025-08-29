53cfeb49d43072154d223a21cffd470a25ecc960
abdullah-al-shishir <abdullahshishir2018@gmail.com>
2025-08-29 17:44:08 +0600

    added token

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const heartCount = document.getElementById("heart-count");
 const heartBtns = document.querySelectorAll(".heart-btn-id");
 const callBtn = document.querySelectorAll(".call-btn");
-
+const Token = document.getElementById("token");
 // Heart Count
 let count = 0;
+let token = 100;
 heartBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     count++;
@@ -15,6 +16,14 @@ heartBtns.forEach((btn) => {
 // Showing Alert when calling
 callBtn.forEach((call) => {
   call.addEventListener("click", () => {
+    if (token > 0) {
+      token = token - 20;
+      Token.innerHTML = token;
+      console.log(token);
+    } else if (token <= 0) {
+      alert("You have no tokens left");
+    }
+
     const card = call.closest(".card");
     const callAlert = card.querySelector(".call-allert");
     const contact = card.querySelector(".contact");
